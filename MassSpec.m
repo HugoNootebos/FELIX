2cd7d2a4c2ed7199763ef915530815eb1f4fc992
Hugo Nootebos <75648039+HugoNootebos@users.noreply.github.com>
2024-01-04 23:01:47 +0100

    Update MassSpec.m

--- MassSpec.m
+++ MassSpec.m
@@ -1,8 +1,8 @@
-function [tof,intensity_set] = MassSpec(file,wavelength)
+function intensity = MassSpec(file,wavelength)
+    plotting = false;
     params = h5read(file,"/Parameters");
     point = abs(wavelength - params.scanStart)/params.scanStep + 1;
     file_char = convertStringsToChars(file);
-    channels = 1;
     if point < 10
         add = "0000";
     elseif point < 100
@@ -16,45 +16,27 @@ function [tof,intensity_set] = MassSpec(file,wavelength)
     end
     lock = true;
     try
-        intensity_set_raw = h5read(file, "/Rawdat/P" + add + point + "_" + num2str(wavelength,'%.4f') + "/Trace");
-        disp("Loading " + add+point+"/"+num2str(wavelength,'%.4f') + " from " + file_char(25:end))
+        data = h5read(file, "/Rawdat/P" + add + point + "_" + num2str(wavelength,'%.4f') + "/Trace");
+        disp("Loading P" + add+point+"/"+num2str(wavelength,'%.4f') + " from " + file_char(25:end))
     catch
-        try
-            point = point - 1;
-            if point < 10
-                add = "0000";
-            elseif point < 100
-                add = "000";
-            elseif point < 1000
-                add = "00";
-            elseif point < 10000
-                add = "0";
-            else
-                add = "";
-            end
-            intensity_set_raw = h5read(file, "/Rawdat/P" + add + point + "_" + num2str(wavelength,'%.4f') + "/Trace");
-            disp("Loading " + add+point+"/"+num2str(wavelength,'%.4f') + " from " + file_char(25:end))
-        catch
-            %continue to next file
-            tof = -1;
-            intensity_set = -1;
-            lock = false;
-        end
+        %ERROR continue to next file
+        intensity = -1;
+        data = -1;
+        lock = false;
     end
     if lock
         tof = [];
-        intensity_set = [];
-        sz = size(intensity_set_raw);
-        for ii = 1:sz(1)
-            background = mean(intensity_set_raw(ii,:),'all');
-            for i = 1:length(intensity_set_raw)
-                tof(end+1) = i;
-                intensity_set(ii,end+1) = intensity_set_raw(i) - background;
-            end
-            % figure();
-            % plot(tof,intensity_set(ii,:))
-            % xlabel("time of flight")
-            % ylabel("intensity")
+        intensity = [];
+        background = mean(data,'all');
+        for i = 1:length(data)
+            tof(end+1) = i;
+            intensity(end+1) = data(i) - background;
         end
+    end
+    if plotting
+        figure();
+        plot(tof,intensity)
+        xlabel("time of flight")
+        ylabel("intensity")
     end
 end

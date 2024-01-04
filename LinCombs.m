b3b724eee6396e630eb8ac3431d47bc5213466ab
Hugo Nootebos <75648039+HugoNootebos@users.noreply.github.com>
2024-01-04 17:06:19 +0100

    Update LinCombs.m

--- LinCombs.m
+++ LinCombs.m
@@ -1,6 +1,8 @@
 function void = LinCombs(product)
-    basis = [194,447, 714, 768, 912, 1120, 1144, 1212, 1244, 1284, 1331, 1377, 1491, 1496, 1522, 1615, 1642, 2996, 3012, 3156, 3189, 3198];
-    err = 50;
+%Useful for predicting anharmonicities. Set basis to the harmonic modes
+%that have significant intensity
+    basis = [194,447, 714.05, 768, 912, 1120, 1144, 1212, 1244, 128.53, 1331, 1377, 1491.29, 1496, 1522, 1615, 1641.64, 2996, 3012, 3156, 3189, 3198];
+    err = 5;
     past_results = [];
     for i = 1:length(basis)
         for ii = 1:length(basis)
@@ -8,11 +10,16 @@ function void = LinCombs(product)
                 results = [basis(i)+basis(ii), basis(i)+basis(iii), basis(ii)+basis(iii), basis(i)+basis(ii)+basis(iii)];
                 for n = 1:length(results)
                     if ~ismember(results(n),past_results)
-                        if results(n) == product
-                            %add if n==1, n==2 etc. for printing values
-                            disp(basis(i) + " + " + basis(ii))
-                        elseif (results(n) - err < product) && (product < results(n) + err)
-                            disp("Found a close match: " + basis(i) + " + " + basis(ii) + "; Error: " + (results(n)-product))
+                        if (results(n) - err < product) && (product < results(n) + err)
+                            if n == 1
+                                disp("Found a match: " + basis(i) + " + " + basis(ii) + "; Error: " + (results(n)-product))
+                            elseif n == 2
+                                disp("Found a match: " + basis(i) + " + " + basis(iii) + "; Error: " + (results(n)-product))
+                            elseif n == 3
+                                disp("Found a match: " + basis(ii) + " + " + basis(iii) + "; Error: " + (results(n)-product))
+                            elseif n == 4
+                                disp("Found a match: " + basis(i) + " + " + basis(ii) + " + " + basis(iii) + "; Error: " + (results(n)-product))
+                            end
                         end
                         past_results = [past_results, results];
                     end

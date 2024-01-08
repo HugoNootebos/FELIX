be5497bd8d0a75473ea9d42b98c2eda9c0a4e3e5
Hugo Nootebos <75648039+HugoNootebos@users.noreply.github.com>
2024-01-08 18:25:59 +0100

    Update PlotFolder.m

--- PlotFolder.m
+++ PlotFolder.m
@@ -1,4 +1,4 @@
-function [wavenumbers, intensity] = PlotFolder(folder, mass)
+function [wavenumbers, intensity_wn] = PlotFolder(folder, mass)
     disp("Reading folder: " + folder)
     int = 6;
     source = dir(folder);
@@ -12,6 +12,7 @@ function [wavenumbers, intensity] = PlotFolder(folder, mass)
     wl = [];
     intensity = [];
     err_count = 0;
+    err_lst = [];
     for file = 1:length(plotFiles)
         params = h5read(plotFiles(file),"/Parameters");
         if params.scanStop > params.scanStart
@@ -20,11 +21,22 @@ function [wavenumbers, intensity] = PlotFolder(folder, mass)
             ud = -1;
         end
         wavelength_set = [];
-        for wli = params.scanStart:ud*params.scanStep:params.scanStop-ud*params.scanStep
+        for wli = params.scanStart : ud*params.scanStep : params.scanStop-ud*params.scanStep
             ms = MassSpec(plotFiles(file), wli);
             if ms == -1
-                disp("Scan ends at " + wli + ". Opening next file")
-                break
+                term = true;
+                %data appears to randomly skip points, fix here
+                for i = -5:1:5
+                   ms = MassSpec(plotFiles(file), wli-i*params.scanStep);
+                   if ms ~= -1
+                       term = false;
+                   end
+                end
+                if term
+                    err_count = err_count+1;
+                    err_lst = [err_lst,file];
+                    break
+                end
             end
             %integrate intensity of +- int points around mass tof
             integral = 0;
@@ -34,20 +46,22 @@ function [wavenumbers, intensity] = PlotFolder(folder, mass)
             intensity(end+1) = -integral;
             wl(end+1) = wli;
         end
-        if isempty(Readh5(plotFiles(file),mass))
-            err_count = err_count + 1;
-        end
     end
     %wl = wl/2;
     wavenumbers = 1./(wl.*10^(-7));
     disp(length(plotFiles) - err_count + "/" + length(plotFiles) + " files plotted successfully")
+    disp(err_lst)
+    [wl, I] = sort(wl);
+    intensity = intensity(I);
+    [wavenumbers, I2] = sort(wavenumbers);
+    intensity_wn = intensity(I2);
     figure('name',"cm" + folder);
-    plot(wavenumbers,intensity)
+    plot(wavenumbers,intensity_wn)
     xlabel("Wavenumbers (cm-1)")
     ylabel("Intensity (arbitrary units)")
     grid on
     grid minor
-    set(gca, 'XDir','reverse')
+    %set(gca, 'XDir','reverse')
     exportgraphics(gcf,"PlotFolder_out_cm.png");
     figure('name',"nm" + folder);
     plot(wl,intensity);
@@ -56,6 +70,6 @@ function [wavenumbers, intensity] = PlotFolder(folder, mass)
     grid on
     grid minor
     exportgraphics(gcf,"PlotFolder_out_nm.png");
-    out = [wavenumbers;intensity];
+    out = [wavenumbers;intensity_wn];
     writematrix(out,"out.csv")
 end

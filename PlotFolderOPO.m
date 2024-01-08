6f7aca319c819c6426c1df89925aa32bc4d88b06
Hugo Nootebos <75648039+HugoNootebos@users.noreply.github.com>
2024-01-08 17:49:31 +0100

    Update PlotFolderOPO.m

--- PlotFolderOPO.m
+++ PlotFolderOPO.m
@@ -1,7 +1,19 @@
-function [wavenumbers,intensity] = PlotFolderOPO(folder, mass)
+function [wavenumbers,dep] = PlotFolderOPO(folder, mass)
     strt = [3100, 3100, 3060, 3060, 3020, 3020, 3140, 3140, 2940, 2940, 2900, 2900, 2980]; %magic numbers. The wavenumber at which each scan starts. IN ORDER
     pts = 334; %Number of points in each scan
     rng = 40; %Wavenumbers covered in one scan
+    int = 6; %width of integration
+    smoothening = true; %set plotAll to false
+    plotAll = false; %for plotting all channels
+    basis = [];
+    doubles = [];
+    for i = 1:length(strt)
+        if ismember(strt(i),basis) 
+            doubles(end+1) = i;
+        else
+            basis(end+1) = strt(i);
+        end
+    end
     disp("Reading folder: " + folder)
     source = dir(folder);
     plotFiles = "";
@@ -11,29 +23,65 @@ function [wavenumbers,intensity] = PlotFolderOPO(folder, mass)
         end
     end
     plotFiles(1) = [];
-    range = [];
-    intensity = [];
-    err_count = 0;
-    for ii = 1:length(plotFiles)
-        [void, new_intensity] = Readh5(plotFiles(ii),mass);
-        intensity = [intensity, new_intensity];
-        if isempty(Readh5(plotFiles(ii),mass))
-            err_count = err_count + 1;
-        end
-    end
+    channel1 = [];
+    channel2 = [];
     wavenumbers = [];
+    dep = [];
+    lock = true;
     for i = 1:length(plotFiles)
         for pt = 1:pts
             wavenumbers(end+1) = strt(i)-(pt*rng/pts);
+            ms = MassSpecOPO(plotFiles(i),pt);
+            if ms == -1
+                lock = false;
+            end
+            if lock
+                channel1 = ms(1,:);
+                channel2 = ms(2,:);
+                integral1 = 0;
+                integral2 = 0;
+                for ii = -int:1:int
+                    integral1 = integral1 + channel1(mass+ii);
+                    integral2 = integral2 + channel2(mass+ii);
+                end
+                dep(end+1) = -log(integral1/integral2);
+            else
+                lock = true;
+            end
         end
     end
+    dep2 = [];
+    wn2 = [];
+    for i = 2:length(doubles)+1
+        dep2 = [dep2,dep(pts*(i-1):pts*(i))];
+        wn2 = [wn2,wavenumbers(pts*(i-1):pts*(i))];
+        dep(pts*(i-1):pts*(i)) = [];
+        wavenumbers(pts*(i-1):pts*(i)) = [];
+    end
     [wavenumbers, I] = sort(wavenumbers);
-    intensity = intensity(I);
-    disp(length(plotFiles) - err_count + "/" + length(plotFiles) + " files plotted successfully")
+    dep = dep(I);
+    [wn2, I2] = sort(wn2);
+    dep2 = dep2(I2);
+    if smoothening
+        for i = 1:length(wn2)
+            for ii = 1:length(wavenumbers)
+                if wn2(i) == wavenumbers(ii)
+                    dep(ii) = (dep2(i)+dep(ii))/2;
+                end
+            end
+        end
+        dep = smooth(dep);
+    end
     figure('name',folder);
-    plot(wavenumbers,intensity)
+    plot(wavenumbers,dep)
+    hold on
+    if plotAll
+        plot(wn2,dep2)
+    end
     xlabel("Wavenumbers (cm-1)")
-    ylabel("Depletion (arbitrary units)")
+    ylabel("Cross section (-ln(dep))")
     grid on
     grid minor
+    exportgraphics(gcf,"PlotFolderOPO_out.png");
+    hold off
 end
